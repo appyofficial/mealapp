102deae1283e63741e9165218f1f0748be115e46
Appy Sharma <appy70890@gmail.com>
2020-04-18 21:19:59 +0200

    Update CategoriesScreen.jsx

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
-const CategoriesScreen = () => {
+const CategoriesScreen = (props) => {
   return (
     <View style={styles.div}>
       <Text>This is Categories Screen. HEY.</Text>
+      <Button
+        title="Go to meals list"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "categoryMeal" });
+        }}
+      />
     </View>
   );
 };
