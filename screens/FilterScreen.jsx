5d95097ff76a7b6f28dc11ece4c47791abcdd4c4
Appy Sharma <appy70890@gmail.com>
2020-05-03 14:28:02 +0200

    filter screen

--- screens/FilterScreen.jsx
+++ screens/FilterScreen.jsx
@@ -1,11 +1,46 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Switch } from "react-native";
 import { HeaderButton } from "../components";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterSwitch}>
+      <Text>{props.label}</Text>
+      {/*thumbColor, trackColor props for switch */}
+      <Switch value={props.value} onValueChange={props.onChange} />
+    </View>
+  );
+};
+
 const FilterScreen = () => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
   return (
     <View style={styles.div}>
       <Text>This is CFilterScreen.</Text>
+      <FilterSwitch
+        label="Gluten free"
+        value={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label="Lactose free"
+        value={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        value={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label="Vegeterian"
+        value={isVegetarian}
+        onChange={(newValue) => isVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -28,6 +63,13 @@ FilterScreen.navigationOptions = (navData) => ({
 const styles = StyleSheet.create({
   div: {
     backgroundColor: "lightblue",
+  },
+  filterSwitch: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    alignItems: "center",
   },
 });
 
